0246e08f50f04d61769a74b3f9d130df70d5fbb9
TPJavaWorld <34994094+TPJavaWorld@users.noreply.github.com>
2018-01-01 10:00:14 +0530

    Spring Demo Project
    Demo project on Spring boot

---
+++ SpingBootDemoProject/src/main/java/com/example/demo/SpingBootDemoProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBootDemoProjectApplication.class, args);
+	}
+}
